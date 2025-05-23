230e5de1b3488c389668c71f17520526842afa39
anotherkimhom <jinhong.jiang1998@gmail.com>
2025-05-23 21:41:38 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-app';
+  @ViewChild('startStop', { static: true })
+  startStop!: ElementRef<SVGStopElement>;
+
+  contractionStarted = false;
+  onLightClick(event: MouseEvent) {
+    console.log('Center button clicked');
+    this.contractionStarted = !this.contractionStarted;
+    if (this.contractionStarted) {
+      // Start the SVG animation
+      const animateElem = this.startStop.nativeElement.querySelector('animate');
+      if (animateElem && typeof animateElem.beginElement === 'function') {
+        animateElem.beginElement();
+      }
+    }
+  }
 }
